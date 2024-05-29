2aee8f8b3129d7a92b149b953a6cdd96e486ec7c
Johnvilin-Sibina <johnvilinsibina@gmail.com>
2024-05-29 18:20:36 +0530

    Endpoint added

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ app.use(express.json());
 //routes
 app.get('/',(req,res)=>{
     // res.status(200).json({message:"Hello"});
-    res.status(200).send("Hello All!")
+    res.status(200).send(`<span style="background-color:aqua;font_size:75px">Welcome to first NodeJS app</span>`)
+})
+
+app.get('/data',(req,res)=>{
+    res.status(200).send(`<span style="background-color:aqua;font_size:75px">Welcome to first endpoint /data</span>`)
 })
 
 //running the port
